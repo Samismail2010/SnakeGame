d3edbdcf88069883c66219a7ab62406ce357497f
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2023-01-26 02:13:06 -0600

    set up gamepanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,33 @@
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
     GamePanel(){
+
+    }
+    public void startGame() {
+        
+    }
+    public void paintComponent(Graphics g) {
+        
+    }
+    public void draw(Graphics g) {
+        
+    }
+    public void move() {
+        
+    }
+    public void checkApple() {
+        
+    }
+    public void checkCollision() {
+        
+    }
+    public void gameOver(Graphics g) {
         
     }
     @Override
@@ -12,5 +35,8 @@ public class GamePanel extends JPanel implements ActionListener {
         // TODO Auto-generated method stub
         
     }
-    
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {}
+    }
 }
