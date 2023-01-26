1adfa6492609a8c2a13dd9b6ff865d866b8eaa54
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2023-01-26 03:24:56 -0600

    created apple

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,20 +3,56 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
-    GamePanel(){
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    //how large the objects are
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    //hold x coord for snake
+    final int x[] = new int [GAME_UNITS];
+    //hold y coords for snake
+    final int y[] = new int [GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+    
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     public void startGame() {
-        
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
     public void paintComponent(Graphics g) {
-        
+        super.paintComponent(g);
+        draw(g);
     }
     public void draw(Graphics g) {
-        
+        g.setColor(Color.green);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+
+    }
+    public void newApple() {
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))* UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))* UNIT_SIZE;
     }
     public void move() {
         
